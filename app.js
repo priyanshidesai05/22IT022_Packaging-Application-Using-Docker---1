cb7f7716de163df360ac906c6a52d7357be2d810
priyanshidesai05 <147694431+priyanshidesai05@users.noreply.github.com>
2024-09-06 22:40:10 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const redis = require('redis');
+
+const app = express();
+const client = redis.createClient({
+    host: 'redis-server',
+    port:6379,
+});
+client.set('visits',0);
+app.get('/',(req,res)=>{
+
+    client.get('visits',(err,visits)=>{
+        res.send('the number of times page was visited is : '+visits);
+        client.set('visits',parseInt(visits)+1);
+    });
+});
+app.listen(8080,()=>{
+    console.log('listining on the port 8080');
+});
